f8618c6fa948bca5fb6629b90c8133203f26211f
Nikita <97m.nik@gmail.com>
2020-12-10 13:56:51 +0300

    Initial Commit

---
+++ trafficLightSwiftUi/trafficLightSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  trafficLightSwiftUiApp.swift
+//  trafficLightSwiftUi
+//
+//  Created by Никита on 10.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct trafficLightSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
